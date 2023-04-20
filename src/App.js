bf9598fa519d4cfccceae25762a59bcbda36d86a
Hitesh <hp212212212@gmail.com>
2023-04-19 18:28:53 -0600

    first commit - 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useTranslation, Trans } from 'react-i18next';
+import i18next from 'i18next';
+import Cookies from 'js-cookie';
+
+const languages = [
+
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  },
+  {
+    code: 'fr',
+    name: 'Français',
+    country_code: 'fr',
+  },
+  {
+    code: 'ar',
+    name: 'العربية',
+    dir: 'rtl',
+    country_code: 'sa',
+  },
+  {
+    code: 'gu',
+    name: 'ગુજરાતી',
+    country_code: 'in',
+  },
+]
 
 function App() {
+  const currentLanguageCode = Cookies.get('i18next') || 'en'
+  const [lo, setLo] = useState('')
+  const [Live, setLive] = useState('')
+  const [LiveText, setLiveText] = useState('')
+  const { t } = useTranslation();
+  const releaseDate = new Date('2020-03-07');
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
+
+  function geoFindMe() {
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      setLo("");
+      setLive(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`)
+      setLiveText(`Latitude: ${latitude} °, Longitude: ${longitude} °`);
+    }
+    function error() {
+      setLo("Unable to retrieve your location")
+    }
+    if (!navigator.geolocation) {
+      setLiveText("Geolocation is not supported by your browser")
+    } else {
+      setLo("Locating…")
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+
+
+  useEffect(() => {
+    currentLanguageCode === 'ar' ?
+      (document.body.dir = 'rtl') : (document.body.dir = 'ltr');
+    document.title = t('app_title')
+  }, [currentLanguageCode])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Title : {t('app_title')}</h2>
+      <h2>language : {t('language')}</h2>
+      <h2>welcome message : {t('welcome_message')}</h2>
+      <h2>days since release : {t('days_since_release', { number_of_days })}</h2>
+
+      {
+        languages.map((res, index) => {
+          return (
+            <button key={index} onClick={() => {
+              i18next.changeLanguage(res.code);
+              // setcurrentLanguageCode(res.code)
+            }}>
+              {res.name}
+            </button>)
+        })
+      }
+
+      <button id="find-me" onClick={geoFindMe}>Show my location</button><br />
+      <p id="status">{lo}</p>
+      <a id="map-link" target="_blank" href={Live}>{LiveText}</a>
+      <h1>Jay Swaminarayan</h1>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // passes language detector down to react-i18next
+  .use(HttpApi) // passes http backend down to react-i18next
+  .init({
+    supportedLngs: ['en', 'ar', 'fr', 'gu'],
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+
+    // lng: "en", // if you're using a language detector, do not define the lng option
+    // lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    detection: {
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
+      caches: ['localStorage', 'cookie'],
+    },
+    backend: {
+      loadPath: 'assets/locales/{{lng}}/translation.json',
+    },
+    // react: { useSuspense: false },n
+  });
+
+// function App() {
+//   const { t } = useTranslation();
+
+//   return <h2>{t('Welcome to React')}</h2>;
+// }
+
+
+
+
+
+const lodingMarkup = (
+  <h1>Loading...................</h1>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={lodingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
