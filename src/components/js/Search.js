166c3ff27a8ff97a2cc2bb928655fe513c86005b
Hitesh <hp212212212@gmail.com>
2023-04-25 19:19:42 -0600

    next - 13

--- src/components/js/Search.js
+++ src/components/js/Search.js
@@ -10,12 +10,14 @@ export default function Search() {
     let kaka = t('Placeholder')
     const [InputCity, setInputCity] = useState('')
     const [Data, setData] = useState([])
+    const [SelectedIndex, setSelectedIndex] = useState(0)
     const [placeholder, setPlaceholder] = useState()
     let [ScaleUl, setScaleUl] = useState('scaleY(1)')
     const { Latitude, setLatitude, Longitude, setLongitude, setCityName, LangCode } = useContext(Coordinates)
     const Search = async () => {
         try {
-            const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${InputCity.length > 2 ? InputCity : ''}&language=${LangCode}&key=b257c0dae6134b9683a9d59a1a1bcb8c&limit=7&language=en&debounce=250`);
+            setSelectedIndex(0)
+            const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${InputCity.length > 2 ? InputCity : ''}&language=${LangCode}&key=b257c0dae6134b9683a9d59a1a1bcb8c&limit=7&language=en&debounce=250&no_dedupe=1&no_annotations=1&population=100000`);
             const data = await response.json();
             setData(data.results)
         }
@@ -25,13 +27,40 @@ export default function Search() {
     }
 
     const Check = (e) => {
+        switch (e.key) {
+            case 'ArrowUp':
+                setSelectedIndex((prevIndex) =>
+                    prevIndex === 0 ? Data.length - 1 : prevIndex - 1
+                );
+                break;
+            case 'ArrowDown':
+                setSelectedIndex((prevIndex) =>
+                    prevIndex === Data.length - 1 ? 0 : prevIndex + 1
+                );
+                break;
+            case 'Tab' || 'Enter':
+                setPlaceholder(Data[0].formatted)
+                setCityName(Data[0].formatted)
+                setLongitude(Data[0].geometry.lng)
+                setLatitude(Data[0].geometry.lat)
+                setInputCity('')
+                break;
+            default:
+                break;
+        }
+
+
         if (e.key === 'Tab' || e.key === 'Enter') {
-            setPlaceholder(Data[0].formatted)
-            setCityName(Data[0].formatted)
-            setLongitude(Data[0].geometry.lng)
-            setLatitude(Data[0].geometry.lat)
+            setPlaceholder(Data[SelectedIndex].formatted)
+            setCityName(Data[SelectedIndex].formatted)
+            setLongitude(Data[SelectedIndex].geometry.lng)
+            setLatitude(Data[SelectedIndex].geometry.lat)
             setInputCity('')
         }
+    }
+
+    const MoseMove = (index) => {
+        setSelectedIndex(index)
     }
 
 
@@ -70,7 +99,16 @@ export default function Search() {
                         {
                             Data.map((item, index) => {
                                 return (
-                                    <li key={index} onClick={() => Display(item)} >{item.formatted}</li>
+                                    <li
+                                        key={index}
+                                        onClick={() => Display(item)}
+                                        onMouseEnter={() => MoseMove(index)}
+                                        className={
+                                            (index === SelectedIndex) ? 'selected' : ''
+                                        }
+                                    >
+                                        {item.formatted}
+                                    </li>
                                 )
                             })
                         }
